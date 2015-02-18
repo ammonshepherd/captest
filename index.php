f26a2c98ee68a4b95460e5ec74d2c4f780c0ca28
Ammon Shepherd <ammon.shepherd@gmail.com>
2015-02-18 10:36:50 -0500

    fix title, add styling to p

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php 
+$title = "Captest Site";
 $message = "Testing site for capistrano deployments"; 
-
 $server = "vagrant, captest, ip: 192.168.33.10"; 
 
 ?>
@@ -15,11 +15,18 @@ $server = "vagrant, captest, ip: 192.168.33.10";
             border: double white;
             padding: 0.4em;
         }
+        p {
+            color: white;
+            font-size: 2em;
+            background-color: #34ad9f;
+            padding: 20px;
+        }
     </style>
 <body>
 
-<h1><?php echo $message; ?></h1>
+<h1><?php echo $title; ?></h1>
 
+<p><?php echo $message; ?></p>
 <p><?php echo $server; ?></p>
 </body>
 </html>
