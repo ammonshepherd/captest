3822b92c5ec88b7f89fac4c4ce9c884651742af6
Ammon Shepherd <ammon.shepherd@gmail.com>
2015-02-18 10:31:46 -0500

    update index file

--- index.php
+++ index.php
@@ -1,5 +1,24 @@
-<?php
+<?php 
+$message = "Testing site for capistrano deployments"; 
 
-echo "<h1>Testing site for capistrano deployments</h1>";
+$server = "vagrant, captest, ip: 192.168.33.10"; 
 
 ?>
+<html>
+    <style>
+        body {#58a389;}
+        h1 {
+            text-align: center;
+            margin-top: 10%;
+            color: white;
+            font-size: 10em;
+            border: double white;
+            padding: 0.4em;
+        }
+    </style>
+<body>
+
+<h1><?php echo $message; ?></h1>
+
+</body>
+</html>
