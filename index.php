1f9918906510ba04245944f62095b833967fd2e6
Ammon Shepherd <ammon.shepherd@gmail.com>
2015-03-02 13:55:39 -0500

    change colors; add wrap div

--- index.php
+++ index.php
@@ -7,16 +7,20 @@ $server = "vagrant, captest, ip: ".$_SERVER['SERVER_ADDR'];
 <html>
     <style>
         body {background-color: #faa32d;}
+        #wrap{
+            background-color: white;
+            margin: auto;
+            }
         h1 {
             text-align: center;
             margin-top: 10%;
-            color: white;
+            color: blue;
             font-size: 6em;
-            border: double white;
+            border: double gray;
             padding: 0.4em;
         }
         p {
-            color: white;
+            color: blue;
             font-size: 2em;
             background-color: #34ad9f;
             padding: 20px;
@@ -24,9 +28,13 @@ $server = "vagrant, captest, ip: ".$_SERVER['SERVER_ADDR'];
     </style>
 <body>
 
-<h1><?php echo $title; ?></h1>
+<div id="wrap">
+    <h1><?php echo $title; ?></h1>
 
-<p><?php echo $message; ?></p>
-<p><?php echo $server; ?></p>
+    <p><?php echo $message; ?></p>
+    <p><?php echo $server; ?></p>
+</div>
+
+
 </body>
 </html>
